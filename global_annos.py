b0a54480e8542fbcf93490c0c57fcf3d578afa47
menjingru <62264914+menjingru@users.noreply.github.com>
2023-11-17 01:38:36 +0800

    Update global_annos.py
    debug

--- global_annos.py
+++ global_annos.py
@@ -1,6 +1,7 @@
 ## 公共区域函数
 import pandas as pd
 import numpy as np
+from global_ import new_bbox_annos_path
 
 def annos():  # 收集有结节图的名字
     annos = pd.read_excel(new_bbox_annos_path)  # 读取bbox_annos.xls
